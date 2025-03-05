fae8fac4bb53b9a951eea92cf023075f097a5164
Valentin Gurduza <valic2004@gmail.com>
2025-03-05 21:24:10 +0200

    adaugare-bundle

---
+++ eUseControl.BeekeepingStore/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+public class BundleConfig
+{
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+        bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"));
+        bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js"));
+        bundles.Add(new StyleBundle("~/Content/css").Include(
+            "~/Content/bootstrap.css",
+            "~/Content/site.css"));
+    }
+}
