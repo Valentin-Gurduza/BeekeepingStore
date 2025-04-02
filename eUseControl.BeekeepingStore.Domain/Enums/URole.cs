040f22cc00badcc447bc346a9730a9926f3b8dbc
Valentin Gurduza <valentin.gurduza@isa.utm.md>
2025-04-02 18:12:00 +0300

    EntityFramework nuget packages, DB

---
+++ eUseControl.BeekeepingStore.Domain/Enums/URole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BeekeepingStore.Domain.Enums
+{
+public enum URole
+    {
+    None = 0,
+    Banned = 1,
+    Deleted = 3,
+    User = 100,
+    Moderator = 200,
+    Vip = 300,
+    Administrator = 400;
+
+    }
+}
